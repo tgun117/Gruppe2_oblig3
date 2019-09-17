9167c65a25db7ed22c177533b5c3782f5b8c9fc1
Bagera22 <b-17_h@hotmail.com>
2019-09-17 13:50:18 +0200

    test
    test

--- dk-a3temp-master/src/main/java/no/ntnu/datakomm/a3/MyClass.java
+++ dk-a3temp-master/src/main/java/no/ntnu/datakomm/a3/MyClass.java
@@ -11,7 +11,7 @@ public class MyClass {
     }
 
     private static void feature2() {
-        // TODO - print "Feature #2"
+        System.out.println("hei");
     }
 
     private static void feature3() {

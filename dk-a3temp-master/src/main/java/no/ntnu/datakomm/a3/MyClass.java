fdc20463182a9b69d492e6c6d5923a4124e90774
Bagera22 <b-17_h@hotmail.com>
2019-09-17 12:53:48 +0200

    testing
    testing

--- dk-a3temp-master/src/main/java/no/ntnu/datakomm/a3/MyClass.java
+++ dk-a3temp-master/src/main/java/no/ntnu/datakomm/a3/MyClass.java
@@ -7,7 +7,7 @@ package no.ntnu.datakomm.a3;
 public class MyClass {
 
     private static void feature1() {
-        // TODO - print "Feature #1"
+        // TODO - print "Feature dsdasdsdasdsadasdasdadadadsadsadsadadadadada#1"
     }
 
     private static void feature2() {
